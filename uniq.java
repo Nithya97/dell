bb46c167a14665b3754299216c22713603925ac9
Nithya97 <nithyalachu97@gmail.com>
2017-06-05 22:05:50 +0530

    Create uniq.java

---
+++ uniq.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class uniq
+{public static void main(String args[])
+{int s=0;
+int a[]=new int[args.length];
+for(int i=0;i<args.length;i++)
+{
+a[i]=Integer.parseInt(args[i]);
+}
+for(int j=0;j<a.length;j++)
+{s=0;
+for(int k=j+1;k<a.length;k++)
+{if(s==0)
+{if(a[j]==a[k])
+s=1;}}
+if(s==0)
+{
+System.out.println(a[j]);
+break;}
+}}}
